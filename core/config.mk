GZOSP/build

repair sepolicy link to vendor
Change-Id: Ibb38ad06b3ce6b9ce87021e93647f7a18cdc8663

core/config.mk
@@ -973,7 +973,7 @@ endif
 ifneq ($(GZOSP_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/gzosp/sepolicy/common/sepolicy.mk)
+$(eval include vendor/gzosp/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk

Ninja-OS/android_build

core/config.mk
@@ -973,7 +973,7 @@ endif
 ifneq ($(NinjaOS_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/ninja/sepolicy/common/sepolicy.mk)
+$(eval include vendor/ninja/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk