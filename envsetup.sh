GZOSP/build

envsetup: correct path to repopick in vendor
 * needs a total overhaul

Change-Id: I2636d8028ffb0c2680dbfa9ec8f8bc80e13a5945

envsetup.sh
@@ -1640,7 +1640,7 @@ function reposync() {
 
 function repopick() {
     T=$(gettop)
-    $T/build/tools/repopick.py $@
+    $T/vendor/gzosp/build/tools/repopick.py $@
 }
 
 function fixup_common_out_dir() {

Ninja-OS/android_build

envsetup.sh
@@ -1656,7 +1656,7 @@ function reposync() {
 
 function repopick() {
     T=$(gettop)
-    $T/build/tools/repopick.py $@
+    $T/vendor/ninja/build/tools/repopick.py $@
 }
 
 function fixup_common_out_dir() {